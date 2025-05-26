b2881fe982131604f8582c79d04f6832ae89866a
Jodica Alyasa Restama <jodica23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:27 +0700

    produk sudah oke by jodica

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayouts'
+import ProductManagement from './pages/Product'
 
 export default function App() {
   return (
    <Routes>
     <Route element={<MainLayout/>}>
     <Route path="/" element={<Dashboard/>}/>
+    <Route path="/produk" element={<ProductManagement/>}/>
     </Route>
    </Routes>
   )
