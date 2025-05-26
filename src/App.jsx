00bbc7f68a17b21135f2d61499d0072e92b2e0a0
Wahyu <wahyu23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:35 +0700

    halaman penjualan by wahyu

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayouts'
+import SalesManagement from './pages/SalesManagement'
 
 export default function App() {
   return (
    <Routes>
     <Route element={<MainLayout/>}>
     <Route path="/" element={<Dashboard/>}/>
+    <Route path="/penjualan" element={<SalesManagement/>}/>
     </Route>
    </Routes>
   )
