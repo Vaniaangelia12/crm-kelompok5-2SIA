abbb397da2ff755836f21f00af35b8304d8cbc77
Jodica <jodica23si@mahasiswa.pcr.ac.id>
2025-06-16 15:43:29 +0700

    Membuat halaman profile dan UserDummy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainLayout from './components/MainLayouts'
 import ProductManagement from './pages/Product'
 import UmpanBalik from './pages/UmpanBalik'
 import FAQ from './pages/FAQ'
+import Profile from './pages/Profil'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
     <Route path="/produk" element={<ProductManagement/>}/>
     <Route path="/umpanbalik" element={<UmpanBalik/>}/>
     <Route path="/faq" element={<FAQ/>}/>
+    <Route path="/akun" element={<Profile/>}/>
     </Route>
    </Routes>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const menuItems = [
 ]
 
 const accountItems = [
-  { name: 'Pengaturan Akun', icon: <Settings />, path: '/akun' },
+  { name: 'Profil', icon: <Settings />, path: '/akun' },
   { name: 'Sign In', icon: <LogIn />, path: '/signin' },
   { name: 'Sign Up', icon: <UserPlus />, path: '/signup' },
 ]
