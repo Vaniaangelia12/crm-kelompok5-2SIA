ba68e426e76955b9d02d24fa72be3d13e5aee6a6
Vania Febri Angelia <vania23si@mahasiswa.pcr.ac.id>
2025-06-18 07:26:54 +0700

    Sidebar tidak ikut terscroll saat halaman discroll

--- src/components/MainLayouts.jsx
+++ src/components/MainLayouts.jsx
@@ -6,10 +6,13 @@ export default function MainLayout() {
   return (
     <div id="app-container" className="bg-gray-100 min-h-screen flex w-full">
       {/* Sidebar (fixed width) */}
-      <Sidebar />
+      <div className="fixed h-full"> {/* Add fixed and h-full here */}
+        <Sidebar />
+      </div>
 
       {/* Main Content Area */}
-      <div id="main-content" className="flex-1 flex flex-col">
+      {/* Add pl-64 or a similar left padding to main-content to account for sidebar's width */}
+      <div id="main-content" className="flex-1 flex flex-col pl-64"> {/* Adjust pl-XX based on your sidebar's actual width */}
         <Header />
         <main className="flex-1 overflow-y-auto">
           <div className="p-6">
